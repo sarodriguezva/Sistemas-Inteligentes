36b973da1d49d27e0abe94e8fe8a94f2fd7f9d5c
Santiago Rodríguez Vallejo <sarodriguezva@unal.edu.co>
2022-09-13 22:52:13 -0500

    feat(core): 💎 segmentación y análisis
    - Separación de tiles en archivos separados
    - Exportación de valores de colores a .txt

---
+++ DiamondRush/ImageProcessing.py
@@ -0,0 +1,29 @@
+from math import ceil
+from math import floor
+from keras.utils import load_img
+from keras.utils import img_to_array
+from keras.utils import array_to_img
+
+img = load_img("./static/level1.png")
+img_array = img_to_array(img)
+
+ts = 43.8   #Tile size
+fr = 3      #First row
+lr = 14     #Last row
+fc = 1      #First column
+lc = 9      #Last column
+
+#Región Jugable
+region = img_array[ceil(ts*fr):floor(ts*lr), ceil(ts*fc):floor(ts*lc)]
+region_img = array_to_img(region)
+region_img.show()
+
+def generateTiles():
+    for i in range(3, 14):
+        for j in range(1, 9):
+            region = img_array[ceil(ts * i):floor(ts * (i+1)), ceil(ts * j):floor(ts * (j+1))]
+            region_img = array_to_img(region)
+            region_img.save("tile" + str(i) + "-" + str(j) + ".png")
+
+#3-4 4-5 5-6 6-7 7-8 8-9 9-10 10-11 11-12 12-13 13-14 ROWS
+#1-2 2-3 3-4 4-5 5-6 6-7 7-8 8-9 COLUMNS
---
+++ DiamondRush/ImageProcessing2.py
@@ -0,0 +1,47 @@
+import numpy as np
+from keras.utils import load_img
+from keras.utils import img_to_array
+
+def generateFile(array):
+    file = open("./static/level1.txt", "w")
+    for row in array:
+        for pix in row:
+            file.write(str(row[pix]) + " ")
+        file.write("\n\n")
+    file.close()
+
+def coloravg(array):
+    #red = 0
+    #green = 0
+    #blue = 0
+    color = 0
+    rown, coln, chan = np.shape(array)
+    n = rown * coln
+
+    for row in array:
+        for pix in range(len(row)):
+            color += row[pix][0]
+            #red += row[pix][0]
+            #green += row[pix][1]
+            #blue += row[pix][2]
+    #red = int(red / n)
+    #green = int(green / n)
+    #blue = int(blue / n)
+    #return red, green, blue
+    return color/n
+
+def listColors():
+    file = open("./static/colors2.txt", "w")
+    for i in range(3, 14):
+        for j in range(1, 9):
+            img = load_img("./static/tile" + str(i) + "-" + str(j) + ".png", color_mode="grayscale")
+            img_array = img_to_array(img)
+            #print(img_array[0])
+            color = coloravg(img_array)
+            #file.write("tile " + str(i) + "-" + str(j) + ": " + str(r) + " " + str(g) + " " + str(b) + "\n")
+            #file.write("tile " + str(i) + "-" + str(j) + ": " + str(color) + "\n")
+            file.write(str(color) + "\n")
+    file.close()
+
+
+listColors()
